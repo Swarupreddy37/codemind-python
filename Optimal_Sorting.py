39514bb498459919ed799bbcae7dd964de8b31b0
Swarupreddy37 <119949180+Swarupreddy37@users.noreply.github.com>
2023-06-28 07:31:45 +0530

    t = int(input()) for i in range(t) :     n = int(input())     a = list(map(int,input().split()))     b = a.copy()     b.sort()     if b == a :         print(0)     else :         print(max(a) - min(a))

--- Optimal_Sorting.py
+++ Optimal_Sorting.py
@@ -4,7 +4,7 @@ for i in range(t) :
     a = list(map(int,input().split()))
     b = a.copy()
     b.sort()
-    if a == b :
+    if b == a :
         print(0)
     else :
-        print(max(a) - min(b))
+        print(max(a) - min(a))
